3a7baae70cb27161272c9a71794ff443e528ef2f
Budies-420-Crew <bud.ies420crew@gmail.com>
2025-03-13 20:12:28 -0700

    update

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+function subscribe() {
+    var email = document.getElementById('subscribe-email').value;
+    alert('Thank you for subscribing: ' + email);
+}
+
 window.Jupiter.init({
     displayMode: "widget",
     endpoint: "https://api.mainnet-beta.solana.com",
